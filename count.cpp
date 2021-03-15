7310cd8fdb6156d313f945269dccb8216788781a
Vaibhav <67143938+Vaibhav-afk@users.noreply.github.com>
2021-03-15 14:00:53 +0530

    Added count.cpp

---
+++ count.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int countZeroes(int arr[], int n) {
+    int c=0;
+    for(int i=n-1;i>=0;--i) if(arr[i]==0) c++;
+    return c;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        int ans=countZeroes(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
