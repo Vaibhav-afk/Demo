955ff609fd1879860e612bcf9da15cf40c36ef2e
Darshita-1 <darshitasingh2001@gmail.com>
2021-03-16 16:23:31 +0530

    Updated count

--- count.cpp
+++ count.cpp
@@ -1,9 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int countZeroes(int arr[], int n) {
+int countZeroes(int arr[], int n) 
+{
     int c=0;
-    for(int i=n-1;i>=0;--i) if(arr[i]==0) c++;
+    for(int i=n-1;i>=0;--i)
+    { if(arr[i]==0)
+      { 
+        c++;
+      }
+    }
     return c;
 }
 
